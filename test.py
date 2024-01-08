5391360f48bc0f3c6d39dc3229e9c9edb4b5b1da
dacruzmathis <mathisdaccruz@gmail.com>
2024-01-08 11:09:27 +0100

    excel commit

--- test.py
+++ test.py
@@ -2,17 +2,17 @@ from yahoo_fin.stock_info import *
 import pandas as pd
 
 tickers = ['AAPL', 'MSFT', 'AMZN', 'GOOG', 'META']
+date = '2023-01-01'
 
-def get_ticker_history(ticker, df):
+def get_ticker_history(ticker, date, df):
     print(ticker)
     df_data = get_data(ticker)
     df_data.reset_index(inplace=True)
-    df_data = df_data[df_data['index'] >= '2023-01-01']
+    df_data = df_data[df_data['index'] >= date]
     return pd.concat([df, df_data], axis=0, ignore_index=True)
 
 df = pd.DataFrame()
 for ticker in tickers:
-    df = get_ticker_history(ticker, df)
+    df = get_ticker_history(ticker, date, df)
 
-print(df.head())
-print(df.tail())
+df.to_excel('ticker_history.xlsx')
